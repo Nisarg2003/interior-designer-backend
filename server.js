0cad4d96eac4161a2a19669970fe63a4e29b33ba
Ruds162700 <109850068+Ruds162700@users.noreply.github.com>
2025-06-13 19:56:58 +0530

    Cors added.

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 import express from 'express';
-import dotenv, { config } from 'dotenv';
+import dotenv from 'dotenv';
 import connectDb from './Config/connectDb.js';
-import postRoutes from "./Routes/postRoutes.js"
-import queryRoutes from "./Routes/queryRoutes.js"
+import postRoutes from "./Routes/postRoutes.js";
+import queryRoutes from "./Routes/queryRoutes.js";
+import cors from 'cors';
 
 dotenv.config();
 connectDb();
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use("/api",postRoutes)
-app.use("/api/queries",queryRoutes)
+
+app.use("/api", postRoutes);
+app.use("/api/queries", queryRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
